0515732e39d891a7e7f425f73d0207d16f6f7d14
avamay <avamay481516@gmail.com>
2021-06-01 14:13:12 +0300

    navigation menu.

--- papercity/papercity_app/views.py
+++ papercity/papercity_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'papercity_app/home.html')
+
+
+def books(request):
+    return render(request, 'papercity_app/books.html')

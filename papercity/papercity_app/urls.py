2cf871505ecba55055081253d5ad06655e73991c
gafmyaa <gafmyaa@gmail.com>
2021-06-02 16:46:41 +0300

    database + home(new books)

--- papercity/papercity_app/urls.py
+++ papercity/papercity_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
+from django.views.generic import ListView
+from .models import Books
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', ListView.as_view(queryset=Books.objects.all().order_by('-id')[:5], template_name='papercity_app/home.html')),
+
 ]
--- papercity/papercity_app/views.py
+++ papercity/papercity_app/views.py
@@ -1,5 +1,11 @@
+from django.http import request
 from django.shortcuts import render
+
 
 
 def home(request):
     return render(request, 'papercity_app/home.html')
+
+def book_list(request):
+    return render(request, 'papercity_app/book_list.html')
+

04f233088a9e247f2b7773496bcdac1eef847693
avamay <avamay481516@gmail.com>
2021-06-02 16:45:31 +0300

    navigation menu+new pages.

--- papercity/papercity_app/urls.py
+++ papercity/papercity_app/urls.py
@@ -5,4 +5,13 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
     path('books', views.books, name='books'),
+    path('cat1', views.cat1, name='cat1'),
+    path('cat2', views.cat2, name='cat2'),
+    path('cat3', views.cat3, name='cat3'),
+    path('cat4', views.cat4, name='cat4'),
+    path('cat5', views.cat5, name='cat5'),
+    path('cat6', views.cat6, name='cat6'),
+    path('cat7', views.cat7, name='cat7'),
+    path('cat8', views.cat8, name='cat8'),
+    path('cat9', views.cat9, name='cat9'),
 ]
--- papercity/papercity_app/views.py
+++ papercity/papercity_app/views.py
@@ -7,3 +7,40 @@ def home(request):
 
 def books(request):
     return render(request, 'papercity_app/books.html')
+
+
+def cat1(request):
+    return render(request, 'papercity_app/cat1.html')
+
+
+def cat2(request):
+    return render(request, 'papercity_app/cat2.html')
+
+
+def cat3(request):
+    return render(request, 'papercity_app/cat3.html')
+
+
+def cat4(request):
+    return render(request, 'papercity_app/cat4.html')
+
+
+def cat5(request):
+    return render(request, 'papercity_app/cat5.html')
+
+
+def cat6(request):
+    return render(request, 'papercity_app/cat6.html')
+
+
+def cat7(request):
+    return render(request, 'papercity_app/cat7.html')
+
+
+def cat8(request):
+    return render(request, 'papercity_app/cat8.html')
+
+
+def cat9(request):
+    return render(request, 'papercity_app/cat9.html')
+

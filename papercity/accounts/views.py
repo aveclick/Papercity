cc60c704e710bd4d72693422634d620e6ef12d20
avamay <avamay481516@gmail.com>
2021-06-06 20:17:56 +0300

    registration + login.

--- papercity/accounts/views.py
+++ papercity/accounts/views.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.http import request
+from django.shortcuts import render
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+def profile(request):
+    return render(request, 'accounts/profile.html')

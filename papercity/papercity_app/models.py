bb48e1b059ff3a74570bc5b1a0a24bc015254704
gafmyaa <gafmyaa@gmail.com>
2021-06-16 20:30:25 +0300

    changed classes in models

--- papercity/papercity_app/models.py
+++ papercity/papercity_app/models.py
@@ -9,6 +9,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse("category_list", kwargs={"slug": self.url})
+
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
--- papercity/papercity_app/urls.py
+++ papercity/papercity_app/urls.py
@@ -1,21 +1,20 @@
 from django.urls import path
 from . import views
-from django.views.generic import ListView
-from .models import Books
 
 urlpatterns = [
     path('', views.home, name=''),
     path('home', views.home, name='home'),
     path("search/", views.Search.as_view(), name="search"),
+    path('book_list', views.BookView.as_view()),
+    path('<slug:slug>', views.BookDetailView.as_view(), name="book_detail"),
+    path('<slug:url>/', views.BookView.category_list, name="category_list"),
+    path('review/<int:pk>', views.AddReview.as_view(), name="add_review"),
     path('selection1', views.selection1, name='selection1'),
     path('selection2', views.selection2, name='selection2'),
     path('selection3', views.selection3, name='selection3'),
     path('selection4', views.selection4, name='selection4'),
     path('stocks', views.stocks, name='stocks'),
     path('selection_all', views.selection_all, name='selection_all'),
-    path('book_list', views.BookView.as_view()),
-    path('<slug:url>/', views.BookView.category_list, name="category_list"),
-    path('<slug:slug>', views.BookDetailView.as_view(), name="book_detail"),
-    path('review/<int:pk>', views.AddReview.as_view(), name="add_review"),
+
 
 ]

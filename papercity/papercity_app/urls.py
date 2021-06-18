e23201466d7836786f8e2952e1f150e50667dae6
gafmyaa <gafmyaa@gmail.com>
2021-06-18 18:50:05 +0300

    changed urls

--- papercity/papercity_app/urls.py
+++ papercity/papercity_app/urls.py
@@ -3,18 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name=''),
-    path('home', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path("search/", views.Search.as_view(), name="search"),
-    path('book_list', views.BookView.as_view()),
-    path('<slug:slug>', views.BookDetailView.as_view(), name="book_detail"),
-    path('<slug:url>/', views.BookView.category_list, name="category_list"),
+    path('book_list/', views.BookView.as_view()),
+    path('book_list/<slug:slug>', views.BookDetailView.as_view(), name="book_detail"),
+    path('categories/<slug:url>/', views.BookView.category_list, name="category_list"),
     path('review/<int:pk>', views.AddReview.as_view(), name="add_review"),
-    path('selection1', views.selection1, name='selection1'),
-    path('selection2', views.selection2, name='selection2'),
-    path('selection3', views.selection3, name='selection3'),
-    path('selection4', views.selection4, name='selection4'),
-    path('stocks', views.stocks, name='stocks'),
-    path('selection_all', views.selection_all, name='selection_all'),
+    path('home/selections/selection1', views.selection1, name='selection1'),
+    path('home/selections/selection2', views.selection2, name='selection2'),
+    path('home/selections/selection3', views.selection3, name='selection3'),
+    path('home/selections/selection4', views.selection4, name='selection4'),
+    path('home/stocks', views.stocks, name='stocks'),
+    path('home/selection_all', views.selection_all, name='selection_all'),
 
 
 ]
